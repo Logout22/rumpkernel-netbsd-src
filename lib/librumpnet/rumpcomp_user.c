1e10e82badcfbe5832317975edaf795ec4693786
Martin Unzner <abmelden22@gmx.de>
2014-02-25 11:44:09 +0100

    Removed some debug output

--- lib/librumpnet/rumpcomp_user.c
+++ lib/librumpnet/rumpcomp_user.c
@@ -4,31 +4,25 @@
 #include "rumpcomp_user.h"
 #include "swarm_client_ipc.c"
 
-#define DPRINTF(x) printf x
-
 int rumpcomp_librumpnet_hive_request_port(
         uint16_t port, int32_t *p_bind_result,
         int netbsd_kernel_protocol) {
 	int err;
-    DPRINTF(("NetBSD kernel protocol is %d, Port is %u\n",
-                netbsd_kernel_protocol, port));
 	if ((err = request_hive_bind(netbsd_kernel_protocol, port))) {
-		DPRINTF(("Failed to send bind request: %d\n", err));
+		//DPRINTF(("Failed to send bind request: %d\n", err));
 		return EINVAL;
 	}
 
 	uint32_t ret_msg;
 	if ((ret_msg = rcv_message_type_sock()) != HIVE_BIND_REPLY) {
-		DPRINTF(("Received wrong message header from Hive: %d\n", ret_msg));
+		//DPRINTF(("Received wrong message header from Hive: %d\n", ret_msg));
 		return EINVAL;
 	}
 
 	if ((err = rcv_reply_hive_bind(p_bind_result))) {
-		DPRINTF(("Failed to receive bind result: %d\n", err));
+		//DPRINTF(("Failed to receive bind result: %d\n", err));
 		return EINVAL;
 	}
-
-	DPRINTF(("Hive bind succeeded, result: %d.\n", *p_bind_result));
 	return 0;
 }
 
